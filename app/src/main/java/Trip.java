67a7cf344ea2f096f7818eec5f783b63ec89264b
Bailee Sjostrom <20sjobai@student.mcpsmt.org>
2018-04-09 10:36:17 -0600

    End of Class 04/09/2018: Step 3.1.3.15

---
+++ app/src/main/java/Trip.java
@@ -0,0 +1,59 @@
+import java.util.Date;
+
+public class Trip {
+private String objectId;
+private String name;
+private String description;
+private Date startDate;
+private Date endDate;
+
+    public String getObjectId() {
+        return objectId;
+    }
+
+    public void setObjectId(String objectId) {
+        this.objectId = objectId;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public Date getStartDate() {
+        return startDate;
+    }
+
+    public void setStartDate(Date startDate) {
+        this.startDate = startDate;
+    }
+
+    public Date getEndDate() {
+        return endDate;
+    }
+
+    public void setEndDate(Date endDate) {
+        this.endDate = endDate;
+    }
+
+    public boolean isShared() {
+        return shared;
+    }
+
+    public void setShared(boolean shared) {
+        this.shared = shared;
+    }
+
+    private boolean shared;
+}
